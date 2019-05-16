6221b00ef36d967f7d48623e7b6875a19f69e951
behuFTP <benjamin.dals.hughes@gmail.com>
2019-05-16 11:02:33 +0200

    init commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import "./main.scss";
 
-console.log(2);
-
-var fromAngle, toAngle,
-    fromCoordX, fromCoordY,
-    toCoordX, toCoordY,
-    path, d;
-
 function getMiddle(x1, y1, x2, y2, howFar) {
     return {
         x: ((x1 + x2) / howFar),
@@ -70,25 +63,20 @@ let parties = [
     }
 ];
 
-/*
+const wheelOfFortune = document.getElementById('wheel-of-fortune');
 
-
-*/
-
-function createPie(cx, cy, r, slices) {
-
+function createWheelOfFortune(cx, cy, r, slices) {
     parties.forEach((party, i) => {
         const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
 
-        path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-        fromAngle = i * 360 / slices;
-        toAngle = (i + 1) * 360 / slices;
+        const fromAngle = i * 360 / slices;
+        const toAngle = (i + 1) * 360 / slices;
 
-        fromCoordX = cx + (r * Math.cos(fromAngle * Math.PI / 180));
-        fromCoordY = cy + (r * Math.sin(fromAngle * Math.PI / 180));
-        toCoordX = cx + (r * Math.cos(toAngle * Math.PI / 180));
-        toCoordY = cy + (r * Math.sin(toAngle * Math.PI / 180));
+        const fromCoordX = cx + (r * Math.cos(fromAngle * Math.PI / 180));
+        const fromCoordY = cy + (r * Math.sin(fromAngle * Math.PI / 180));
+        const toCoordX = cx + (r * Math.cos(toAngle * Math.PI / 180));
+        const toCoordY = cy + (r * Math.sin(toAngle * Math.PI / 180));
 
         let middle1 = getMiddle(fromCoordX, fromCoordY, toCoordX, toCoordY, 2);
         let middle = getMiddle(middle1.x, middle1.y, cx, cy, 2);
@@ -97,21 +85,14 @@ function createPie(cx, cy, r, slices) {
         text.setAttributeNS(null, 'x', middle.x - 6);
         text.setAttributeNS(null, 'y', middle.y + 6);
         text.setAttributeNS(null, 'style', `transform: rotate(${fromAngle + 98}deg); transform-origin: ${middle.x}px ${middle.y}px;`);
-
-        circle.setAttributeNS(null, 'cx', middle.x);
-        circle.setAttributeNS(null, 'cy', middle.y);
-        circle.setAttributeNS(null, 'r', 1);
-
-        var txt = document.createTextNode(party.letter);
-
-        text.appendChild(txt);
-        d = 'M' + cx + ',' + cy + ' L' + fromCoordX + ',' + fromCoordY + ' A' + r + ',' + r + ' 0 0,1 ' + toCoordX + ',' + toCoordY + 'z';
-
+        const textNode = document.createTextNode(party.letter);
+        text.appendChild(textNode);
+        
+        const d = 'M' + cx + ',' + cy + ' L' + fromCoordX + ',' + fromCoordY + ' A' + r + ',' + r + ' 0 0,1 ' + toCoordX + ',' + toCoordY + 'z';
         path.setAttributeNS(null, "d", d);
         path.setAttributeNS(null, "fill", party.color);
-        document.getElementById('pie').appendChild(circle);
-        document.getElementById('pie').appendChild(path);
-        document.getElementById('pie').appendChild(text);
+        wheelOfFortune.appendChild(path);
+        wheelOfFortune.appendChild(text);
 
         parties[i].angles = {
             start: fromAngle,
@@ -122,10 +103,7 @@ function createPie(cx, cy, r, slices) {
     });
 }
 
-createPie(55, 55, 50, parties.length);
-
-console.log(parties);
-
+createWheelOfFortune(55, 55, 50, parties.length);
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -133,16 +111,23 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+document.querySelector('button').addEventListener('click', showWinner);
+
 let degrees = 0;
 let winnerParty;
 let setIntervalId;
-document.querySelector('button').addEventListener('click', () => {
+let setTimeoutId;
+let isColored = false;
+const result = document.querySelector('.result');
+
+function showWinner() {
     if (winnerParty) {
         clearInterval(setIntervalId);
         winnerParty.path.setAttributeNS(null, "fill", winnerParty.color);
+        clearTimeout(setTimeoutId);
     }
-    degrees = degrees + getRandomInt(360, 2000);
-    document.getElementById('pie').style = `transform: rotate(${-degrees}deg);`
+    degrees = degrees + getRandomInt(800, 2000);
+    wheelOfFortune.style = `transform: rotate(${-degrees}deg);`
 
     winnerParty = parties.find(party => {
         if ((degrees - 90) % 360 >= party.angles.start && (degrees - 90) % 360 < party.angles.end) {
@@ -150,16 +135,14 @@ document.querySelector('button').addEventListener('click', () => {
         }
     });
 
-    let isColored = false;
-    setTimeout(() => {
+    setTimeoutId = setTimeout(() => {
         setIntervalId = setInterval(() => {
             isColored = !isColored;
-            isColored ? winnerParty.path.setAttributeNS(null, "fill", 'red') : winnerParty.path.setAttributeNS(null, "fill", 'green');
+            isColored ? winnerParty.path.setAttributeNS(null, "fill", 'rgb(255,215,0)') : winnerParty.path.setAttributeNS(null, "fill", '#ae00ff');
         }, 300);
         isColored = !isColored;
-        isColored ? winnerParty.path.setAttributeNS(null, "fill", 'red') : winnerParty.path.setAttributeNS(null, "fill", 'green');
+        isColored ? winnerParty.path.setAttributeNS(null, "fill", 'rgb(255,215,0)') : winnerParty.path.setAttributeNS(null, "fill", '#ae00ff');
         
-        document.querySelector('.result').innerHTML = `Tillykke du skal stemme på <strong>${winnerParty.name} - ${winnerParty.letter} </strong>`;
+        result.innerHTML = `Tillykke du skal stemme på <strong>${winnerParty.name} - ${winnerParty.letter} </strong>`;
     }, 3000);
-});
-
+}
